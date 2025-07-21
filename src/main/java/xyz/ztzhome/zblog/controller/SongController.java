f89c29d780d2033c67936e1e14e6e8d65a355323
zhuzhu888666 <3036875448@qq.com>
2025-07-21 16:44:39 +0800

    更新歌曲操作，完成歌曲上传功能

--- src/main/java/xyz/ztzhome/zblog/controller/SongController.java
+++ src/main/java/xyz/ztzhome/zblog/controller/SongController.java
@@ -1,4 +1,36 @@
 package xyz.ztzhome.zblog.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import xyz.ztzhome.zblog.entity.DTO.AddSongDTO;
+import xyz.ztzhome.zblog.entity.response.ResponseMessage;
+import xyz.ztzhome.zblog.service.impl.SongServiceImpl;
+
+@RestController
+@RequestMapping("/song")
 public class SongController {
+    @Autowired
+    SongServiceImpl songService;
+
+
+    @PostMapping("addSong")
+    public ResponseMessage addSong(@RequestPart("data") String data, @RequestPart("audioFile") MultipartFile audioFile){
+
+        System.out.println(data);
+
+        AddSongDTO addSongDTO = new AddSongDTO();
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            addSongDTO = mapper.readValue(data, AddSongDTO.class);
+
+            return songService.addSong(addSongDTO,audioFile);
+        }catch (Exception e){
+            return new ResponseMessage(0,"接收对象构建失败:"+e.getMessage());
+        }
+    }
 }
--- src/main/java/xyz/ztzhome/zblog/entity/response/ResponseMessage.java
+++ src/main/java/xyz/ztzhome/zblog/entity/response/ResponseMessage.java
@@ -5,7 +5,14 @@ import lombok.Data;
 //@Data自动生成模板get和set
 @Data
 public class ResponseMessage<T> {
-    private Integer code;   // 业务状态码
+    private int code;   // 业务状态码
     private String message; // 提示信息
     private T data;
+
+    public ResponseMessage() {};
+
+    public ResponseMessage(int code, String message) {
+        this.code = code;
+        this.message = message;
+    }
 }
